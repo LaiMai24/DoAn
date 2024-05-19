4173de067ef98c0ba8f54b2e8437cc23211d5a0b
laithimai <laithimai0987944058@gmail.com>
2024-05-19 23:02:50 +0700

    update code

---
+++ TECH/TECH/Controllers/Components/CategoryMenuComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TECH.Areas.Admin.Models;
+using TECH.Service;
+
+namespace TECH.Controllers.Components
+
+{
+    [ViewComponent(Name = "CategoryMenuComponent")]
+    public class CategoryMenuComponent : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryMenuComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {           
+            var categoryModel = _categoryService.GetAllMenu();
+            var data = new List<CategoryModelView>();
+            if (categoryModel != null && categoryModel.Count >0)
+            {
+                var categoryParent = categoryModel.Where(p => p.parentId == null).ToList();
+                if (categoryParent != null && categoryParent.Count > 0)
+                {
+                    foreach (var item in categoryParent)
+                    {
+                        var itemChild = categoryModel.Where(p=>p.parentId == item.id).ToList();
+                        item.LstChildrent = itemChild;
+                        data.Add(item);
+                    }
+                }
+            }
+            return View(data);
+        }
+    }
+}
---
+++ TECH/TECH/Controllers/Components/PostComponent.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TECH.Areas.Admin.Models;
+using TECH.Areas.Admin.Models.Search;
+using TECH.Service;
+
+namespace TECH.Controllers.Components
+
+{
+    //[ViewComponent(Name = "CategoryMenuComponent")]
+    public class PostComponent : ViewComponent
+    {
+        private readonly IPostsService _postsService;
+        public PostComponent(IPostsService postsService)
+        {
+            _postsService = postsService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var postsViewModelSearch = new PostsViewModelSearch();
+            postsViewModelSearch.PageIndex = 1;
+            postsViewModelSearch.PageSize = 10;
+            var data = _postsService.GetAllPaging(postsViewModelSearch);
+            if (data != null && data.Results != null && data.Results.Count > 0)
+            {
+                data.Results = data.Results.Where(p => p.status != 1).ToList();
+                //foreach (var item in data.Results)
+                //{
+                //    //if (item.author.HasValue)
+                //    //{
+                //    //    var appUser = _appUserService.GetByid(item.author.Value);
+                //    //    if (appUser != null && !string.IsNullOrEmpty(appUser.full_name))
+                //    //    {
+                //    //        item.author_name = appUser.full_name;
+                //    //    }
+                //    //}
+
+                //}
+            }
+            return View(data);
+        }
+    }
+}
